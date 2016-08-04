3528b7d5aff3a07da14d9385a874c2e7ceba7417
damcapn <calihistyle101@gmail.com>
2016-08-04 10:21:59 -0600

    deleted the h1 tag
    deleted the h1 tag

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
 <!DOCTYPE html>
 <html>
+
   <head>
+
     <?php require_once("lib/headUtils.php");?>
+
   </head>
+
   <body>
-    <h1>Hello</h1>
+
+
+        
+
+
+
 
     <p>
       Team BT FTW!
     </p>
 
   </body>
+
 </html>
