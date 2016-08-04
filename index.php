411fa3cb00539b3767cd5a7484a2af1b9e52138d
damcapn <calihistyle101@gmail.com>
2016-08-04 09:57:24 -0600

    added a jumbotron in html
    added a jumbotron in html

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html>
   <head>
+
     <meta charset="utf-8">
-    <title></title>
+
+    <title>Team Bad Ass</title>
+
   </head>
+
   <body>
-    <h1>Hello Team BT</h1>
-    <h2>Timmaaaaa</h2>
-    <p>really?></p>
+
+    <div class="jumbotron">
+      <h1>Hello Team BT</h1>
+      <p>...</p>
+      <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+    </div>
+
+        <h2>Timmaaaaay</h2>
+
+
+
   </body>
+
 </html>
